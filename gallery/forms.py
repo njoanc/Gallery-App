554c15ce364de5c609a84f6a9f6b33497d9d0b81
Wecode <wecode@wecode>
2019-03-13 11:17:29 +0200

    fixing intentation error in forms.py

--- gallery/forms.py
+++ gallery/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Feed
+from .models import Gallery
 
 class DocumentForm(forms.ModelForm):
     class Meta:
-        model = Feed
+        model = Gallery
         fields = ('description', 'document', )
