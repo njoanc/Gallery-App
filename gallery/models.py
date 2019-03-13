2d46636135ee0f0bb530f9329ab6cec738a3db79
Wecode <wecode@wecode>
2019-03-13 10:52:58 +0200

    running migrations

--- gallery/models.py
+++ gallery/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Feed(models.Model):
         description = models.CharField(max_length=255, blank=True)
-        document = models.FileField(upload_to='static/documents/')
+        document = models.FileField(upload_to='static/desktop/')
